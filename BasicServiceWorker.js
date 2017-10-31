01fcfe23da8c620d2ca2dc52949868732e9216a0
Flavio Tobler <flavio.to@hispeed.ch>
2017-10-31 15:31:23 +0100

    bug

--- BasicServiceWorker.js
+++ BasicServiceWorker.js
@@ -1,5 +1,5 @@
 self.addEventListener('fetch', function(event){
 	if(/\.jpg$/.test(event.request.url)){
-		event.respondWith(fetch('/images/unicorn.jpg'));
+		event.respondWith(fetch('https://flaviotobler.github.io/iuk_3_m/images/unicorn.jpg'));
 	}
 });
