fefd4d1a5698ce09b1a05f6bb22f55bbd242be76
Flavio Tobler <flavio.to@hispeed.ch>
2017-11-02 16:15:57 +0100

    Upload the Caching Exercise

---
+++ progressive-times-template/progressive-times/sw.js
@@ -0,0 +1,40 @@
+var cacheName = 'latestNews-v1';
+
+// Cachen der Bekannten Ressourcen wärend der Installation
+self.addEventListener('install', event => {
+	event.waitUntil(
+	caches.open(cacheName)
+	.then(cache => cache.addAll([
+		'./js/main.js',
+		'./js/article.js',
+		'./images/newspaper.svg',
+		'./css/site.css',
+		'./data/latest.json',
+		'./data/data-1.json',
+		'./article.html',
+		'./index.html'
+		]))
+	);
+});
+
+// Cachen der noch nicht gespeicherten Ressourcen beim Aufruf
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches.match(event.request, { ignoreSearch: true}).then(function(response){
+			if(response){
+				return response;
+			}
+			var requestToCache = event.request.clone();
+			return fetch(requestToCache).then(function(response){
+				if(!response || response.status !== 200){
+					return response;
+				}
+				var responseToCache = response.clone();
+				caches.open(cacheName).then(function(cache){
+					cache.put(requestToCache, responseToCache);
+				});
+				return response;
+			});
+		})
+	);
+});
