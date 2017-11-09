39b83212b64b8f6628711411a219faf713075c0e
Flavio Tobler <flavio.to@hispeed.ch>
2017-11-09 15:56:21 +0100

    Load up Webp exercice

---
+++ webp-template/webp-template/service-worker.js
@@ -0,0 +1,21 @@
+"use strict";
+
+self.addEventListener('fetch', function(event) {
+	if(/\.jpg$|.png$/.test(event.request.url)){
+		var supportsWebp = false;
+		if(event.request.headers.has('accept')){
+			supportsWebp = event.request.headers
+				.get('accept')
+				.includes('webp');
+		}
+		if(supportsWebp){
+			var req = event.request.clone();
+			var returnUrl = req.url.substr(0, req.url.lastIndex£Of(".")) + ".webp";
+			event.respondWith(
+				fetch(returnUrl, {
+					mode: 'no-cors'
+				})
+			);
+		}
+	}
+});
